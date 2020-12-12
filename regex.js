5f7f8713112d96ac2022e3412d0ccee152800c79
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-12-12 18:45:24 +0900

    Update regex.js

--- regex.js
+++ regex.js
@@ -1,2 +1,10 @@
 const inviteRegex = /(https?:\/\/)?(www\.|canary\.|ptb\.)?discord(\.gg|(app)?\.com\/invite|\.me)\/([^ ]+)\/?/gi;
 const botInvRegex = /(https?:\/\/)?(www\.|canary\.|ptb\.)?discord(app)?\.com\/(api\/)?oauth2\/authorize\?([^ ]+)\/?/gi;
+
+module.exports = class Regex {
+	static stripInvites(str, { guild = true, bot = true, text = '[invite code]' } = {}) {
+		if (guild) str = str.replace(inviteRegex, text);
+		if (bot) str = str.replace(botInvRegex, text);
+		return str;
+	}
+}
