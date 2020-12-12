76295d15706af592b620c8d0c539368f0ff39fc2
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-12-12 18:41:56 +0900

    Create regex.js

---
+++ regex.js
@@ -0,0 +1,2 @@
+const inviteRegex = /(https?:\/\/)?(www\.|canary\.|ptb\.)?discord(\.gg|(app)?\.com\/invite|\.me)\/([^ ]+)\/?/gi;
+const botInvRegex = /(https?:\/\/)?(www\.|canary\.|ptb\.)?discord(app)?\.com\/(api\/)?oauth2\/authorize\?([^ ]+)\/?/gi;
